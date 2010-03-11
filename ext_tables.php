a08596816a13131a3a82dbbb5f0a7ad1d52c17e1
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-11 21:47:53 +0000

    Bugfix for older Typo3 installation, the ext_localconf was included to early, move the alwaysIntegrate to ext_tables
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3jquery/trunk@31040 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -5,5 +5,12 @@ if (!defined ('TYPO3_MODE')) {
 
 if (TYPO3_MODE == 'BE') {
 	t3lib_extMgm::addModule('tools', 'txt3jqueryM1', '', t3lib_extMgm::extPath($_EXTKEY).'mod1/');
+} else {
+	// Add Hooks
+	$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['t3jquery']);
+	if ($confArr['alwaysIntegrate']) {
+		require_once(t3lib_extMgm::extPath('t3jquery').'class.tx_t3jquery.php');
+		tx_t3jquery::addJqJS();
+	}
 }
 ?>
