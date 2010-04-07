81512cf681c5e3c80cd9f7020d0203302f9dc461
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-07 20:26:27 +0000

    Renew the documentation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3jquery/trunk@32007 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,5 @@
 <?php
-/* Add Hooks */
-if (TYPO3_MODE != 'BE' && t3lib_div::compat_version("4.3.0")) {
-	$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['t3jquery']);
-	if ($confArr['alwaysIntegrate']) {
-		require_once(t3lib_extMgm::extPath('t3jquery').'class.tx_t3jquery.php');
-		tx_t3jquery::addJqJS();
-	}
+if (!defined ('TYPO3_MODE')) {
+ 	die ('Access denied.');
 }
 ?>
