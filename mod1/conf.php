e757ce735e07c974107fe76580a68b934d15ddf4
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-08 08:58:55 +0000

    Fix file mismatch in SVN from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3jquery/trunk@33998 735d13b6-9817-0410-8766-e36946ffe9aa

--- mod1/conf.php
+++ mod1/conf.php
@@ -2,7 +2,7 @@
 
 // DO NOT REMOVE OR CHANGE THESE 3 LINES:
 define('TYPO3_MOD_PATH', '../typo3conf/ext/t3jquery/mod1/');
-$BACK_PATH = '../../../../typo3/';
+$BACK_PATH='../../../../typo3/';
 $MCONF['name'] = 'tools_txt3jqueryM1';
 
 $MCONF['access'] = 'user,group';
