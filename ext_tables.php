8937edb0f3a9133efc8b4c4e83d1988ba165f3a5
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-22 21:49:56 +0000

    Fix the back_path in mod1 (#37386) / Create t3jquery.cfg if not exist (#37401)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3jquery/trunk@62725 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,7 @@ if (!defined ('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
+
 if (TYPO3_MODE == 'BE') {
 	// get extension configuration
 	$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['t3jquery']);
@@ -32,6 +33,7 @@ if (TYPO3_MODE == 'BE') {
 	}
 
 	if (! $confArr['integrateFromCDN']) {
+		t3lib_extMgm::addModulePath('tools_txt3jqueryM1', t3lib_extMgm::extPath($_EXTKEY) . 'mod1/');
 		t3lib_extMgm::addModule('tools', 'txt3jqueryM1', '', t3lib_extMgm::extPath($_EXTKEY).'mod1/');
 	}
 }
