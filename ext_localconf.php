9e5be5556c84832da001c4c1f6161935c8d5e92e
Kay Strobach <info@kay-strobach.de>
2015-01-29 14:47:31 +0100

    [TASK] remove 4.3 and below 6.2 compat

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,9 @@
 <?php
 /* Add Hooks */
 if (TYPO3_MODE != 'BE') {
-	if (class_exists(t3lib_utility_VersionNumber)) {
-		$TYPO3_version = t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version);
-	} else {
-		$TYPO3_version = t3lib_div::int_from_ver(TYPO3_version);
-	}
-	if ($TYPO3_version >= 4003000) {
-		$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['t3jquery']);
-		if ($confArr['alwaysIntegrate']) {
-			tx_t3jquery::addJqJS();
-		}
+	$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['t3jquery']);
+	if ($confArr['alwaysIntegrate']) {
+		\T3Ext\T3jquery\Utility\T3jqueryUtility::addJqJS();
 	}
 }
 ?>
