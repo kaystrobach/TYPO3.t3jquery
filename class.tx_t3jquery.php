71939844cd5d80f096cb9b946225b5375a2b411b
Kay Strobach <info@kay-strobach.de>
2015-01-29 16:35:58 +0100

    [TASK] add dummy class.t3jquery.php to avoid fatal errors by inclusion via TS in includeLibs ...

---
+++ class.tx_t3jquery.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * please ensure, that the file is not included in TS directly anymore,
+ * otherwise you will get some serious entries in your deprecation logfile.
+ */
+\TYPO3\CMS\Core\Utility\GeneralUtility::deprecationLog('please remove includeLibs.t3jquery = EXT:t3jquery/class.tx_t3jquery.php from TS');
